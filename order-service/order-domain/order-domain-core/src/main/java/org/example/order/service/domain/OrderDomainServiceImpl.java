2345c03f69792e1676d84adc6d58a47b96d0865f
Yuta <y.sugiyama54@gmail.com>
2023-02-21 00:13:19 +0900

    OrderDomainServiceの実装

---
+++ order-service/order-domain/order-domain-core/src/main/java/org/example/order/service/domain/OrderDomainServiceImpl.java
@@ -0,0 +1,76 @@
+package org.example.order.service.domain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.domain.Exception.DomainException;
+import org.example.order.service.domain.entiry.Order;
+import org.example.order.service.domain.entiry.Product;
+import org.example.order.service.domain.entiry.Restaurant;
+import org.example.order.service.domain.event.OrderCancelledEvent;
+import org.example.order.service.domain.event.OrderCreatedEvent;
+import org.example.order.service.domain.event.OrderPaidEvent;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Slf4j
+public class OrderDomainServiceImpl implements OrderDomainService {
+
+    private static final ZonedDateTime ZONED_DATE_TIME = ZonedDateTime.now(ZoneId.of("UTC"));
+
+    @Override
+    public OrderCreatedEvent validateAndInitiateOrder(Order order, Restaurant restaurant) {
+        validateRestaurant(restaurant);
+        setOrderProductInformation(order, restaurant);
+        order.validateOrder();
+        order.initializeOrder();
+        log.info("Order with id: {} is initiated", order.getId().getValue());
+        return new OrderCreatedEvent(order, ZONED_DATE_TIME);
+    }
+
+    @Override
+    public OrderPaidEvent payOrder(Order order) {
+        order.pay();
+        log.info("Order with id: {} is paid", order.getId().getValue());
+        return new OrderPaidEvent(order, ZONED_DATE_TIME);
+    }
+
+    @Override
+    public void approveOrder(Order order) {
+        order.approve();
+        log.info("Order with id: {} is approved", order.getId().getValue());
+    }
+
+    @Override
+    public OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages) {
+        order.initCancel(failureMessages);
+        log.info("Order payment is cancelling for order id: {}", order.getId().getValue());
+        return new OrderCancelledEvent(order, ZONED_DATE_TIME);
+    }
+
+    @Override
+    public void cancelOrder(Order order, List<String> failureMessages) {
+        order.cancel(failureMessages);
+        log.info("Order with id: {} is cancelled", order.getId().getValue());
+    }
+
+    private void validateRestaurant(Restaurant restaurant) {
+        if (!restaurant.isActive()) {
+            throw new DomainException(String.format("Restaurant with id %s is currently not active.",
+                    restaurant.getId().getValue()));
+        }
+
+    }
+
+    private void setOrderProductInformation(Order order, Restaurant restaurant) {
+        order.getItems().forEach(orderItem -> restaurant.getProducts().forEach(restaurantProduct -> {
+            Product currentProduct = orderItem.getProduct();
+            if (currentProduct.equals(restaurantProduct)) {
+                currentProduct.updateWithConfirmedNameAndPrice(
+                        restaurantProduct.getName(),
+                        restaurantProduct.getPrice()
+                );
+            }
+        }));
+    }
+}
--- order-service/order-domain/order-domain-core/src/main/java/org/example/order/service/domain/entiry/Product.java
+++ order-service/order-domain/order-domain-core/src/main/java/org/example/order/service/domain/entiry/Product.java
@@ -5,11 +5,16 @@ import org.example.domain.valueobject.Money;
 import org.example.domain.valueobject.ProductId;
 
 public class Product extends BaseEntity<ProductId> {
-    private final String name;
-    private final Money price;
+    private String name;
+    private Money price;
 
     public Product(ProductId productId, String name, Money price) {
         super.setId(productId);
+        this.name = name;
+        this.price = price;
+    }
+
+    public void updateWithConfirmedNameAndPrice(String name, Money price) {
         this.name = name;
         this.price = price;
     }
